60df97d2dc80a0e2bf3029176c50055aac3d7918
luciano093 <luparedes100@gmail.com>
2024-11-07 23:58:22 -0500

    add points for individual users

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ const client = new Client({
   ],
 });
 
-let points = 0;
+let everyone = new Map();
 
-everyone = new Map();
-
-client.on('ready', (c) => {
-  console.log(`✅ ${c.user.tag} is online.`);
+client.on('ready', (bot) => {
+  console.log(`${bot.user.tag} is online!`);
 });
 
 client.on('ready', async () => {
   const guild = client.guilds.cache.get(process.env.GUILD_ID);
+
   if (guild) {
+    console.log("test");
     try {
       const members = await guild.members.fetch();
       members.forEach(member => {
@@ -32,26 +32,38 @@ client.on('ready', async () => {
   }
 });
 
-
-
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
+  let user = interaction.user;
+
   if (interaction.commandName === 'points') {
-    return interaction.reply('You currently have ' + points + ' points!');
+    let points = everyone.get(user.username) ?? 0;
+    return interaction.reply(`You currently have ${points} points!`);
   }
   if (interaction.commandName === 'scoreboard') {
+    let members = await interaction.guild.members.fetch();
+
+    for (const [_, member] of members) {
+      if(everyone.get(member.user.username) !== undefined) continue;
+
+      everyone.set(member.user.username, 0);
+    }
+
+
     return interaction.reply("everyone's points: " + JSON.stringify(Array.from(everyone.entries()))); 
   }
 
   if (interaction.commandName === 'more_points') {
     try {
-      everyone.set("daniel_3117", points += 1)
-      const daniel = await client.users.fetch(DANIEL_ID);
-      console.log(`${daniel.username} has been given a point!`);
-      return interaction.reply("You have been given a point " + daniel.username + "!");
+      let previousPoints = everyone.get(user.username) ?? 0;
+
+      everyone.set(user.username, ++previousPoints);
+
+      console.log(`${user.username} has been given a point!`);
+      return interaction.reply("You have been given a point " + user.username + "!");
     } catch (error) {
-      console.error('Error fetching Daniel:', error);
+      console.error('Error fetching user:', error);
       return interaction.reply("An error occurred while fetching the user's information.");
     }
   }
